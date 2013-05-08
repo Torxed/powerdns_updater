aa0ead3cee85297feaf8ae96a9506baa01d9219f
Lord Anton Hvornum <anton.feeds@gmail.com>
2013-05-08 19:17:29 +0200

    Cleaned up output syntax and added a help description on startup

--- server/local_tool.py
+++ server/local_tool.py
@@ -1,6 +1,10 @@
 from sys import version_info, argv
 from backend import SQL
 
+__version__ = '0.1-alpha'
+__authors__ = 'anton@hvornum.se'
+__verified__ = ('3.3')
+
 if version_info.major < 3:
 	_input = raw_input
 elif version_info.major > 2:
@@ -25,8 +29,15 @@ if len(argv) > 1:
 else:
 	backend = SQL()
 
+print('v' + __version__)
+print('Tested on Python versions:' + ' '.join(__verified__))
+print('\tValid options are:')
+print('\tadd [domain|record] - create a record or domain')
+print('\tmodify - modify a record')
+print('\treplace - Replace IP on records\n')
+
 while 1:
-	inp = _input('> ')
+	inp = _input('\n> ')
 	low_inp = inp.lower()
 
 	if low_inp in ('quit', 'exit', 'break', 'die'):
@@ -34,11 +45,11 @@ while 1:
 
 	if low_inp[:3] == 'add':
 		if not 'domain' in low_inp or 'record' in low_inp:
-			low_inp += ' ' + _input('Record or domain?> ')
+			low_inp += ' ' + _input('\nRecord or domain?> ')
 		
 		values = {}
 		print('Enter: "key=value" to commit to the database.')
-		print('When done, write "done" to commit (if any values were submitted)')
+		print('When done, write "done" to commit (if any values were submitted)\n')
 
 		if 'domain' in low_inp:
 			inp_string = 'domain-values'
@@ -51,10 +62,10 @@ while 1:
 			print('\tip=<ip|hostname> (IP for ex A record, hostname for MX records etc)')
 			print('\ttype<record type> (A|MX|CNAME... etc)')
 			print('\tdomain=<domain that the record belongs to>')
-			print('\tprio=<prio for MX record if that\'s the type> (optional)')
+			print('\tprio=<prio for MX record if that\'s the type> (optional)\n')
 
 		while 1:
-			value = _input(inp_string + '> ')
+			value = _input('\n' + inp_string + '> ')
 			if value.lower() == 'done':
 				break
 			elif '=' in value:
@@ -72,21 +83,21 @@ while 1:
 								prio=values['prio'])
 		elif 'domain' in low_inp:
 			if not 'name' in values:
-				print(' [!] At least a name for the domain must be given!')
+				print(' [!] At least a name for the domain must be given!\n')
 			else:
 				backend.add_domain(name=values['name'])
 	elif low_inp[:3] == 'mod' or low_inp[:6] == 'modify':
 		values = {}
 		print('Enter: "key=value" to commit to the database.')
-		print('When done, write "done" to commit (if any values were submitted)')
+		print('When done, write "done" to commit (if any values were submitted)\n')
 
 		print('Valid options are:')
 		print('\tname=<record name>')
 		print('\tip=<ip|hostname> (IP for ex A record, hostname for MX records etc)')
-		print('\trecord=<record type> (A|MX|CNAME... etc)')
+		print('\trecord=<record type> (A|MX|CNAME... etc)\n')
 
 		while 1:
-			value = _input('Modifications> ')
+			value = _input('\nModifications> ')
 			if value.lower() == 'done':
 				break
 			elif '=' in value:
@@ -105,10 +116,10 @@ while 1:
 		print('Valid options are:')
 		print('\told=<old IP address>')
 		print('\nnew=<new ip>')
-		print('\trecord=<record name> (example.com, www.example.com etc)')
+		print('\trecord=<record name> (example.com, www.example.com etc)\n')
 
 		while 1:
-			value = _input('Replacement values> ')
+			value = _input('\nReplacement values> ')
 			if value.lower() == 'done':
 				break
 			elif '=' in value:
@@ -116,6 +127,6 @@ while 1:
 				values[key] = value
 
 		if not 'old' in values or 'new' in values:
-			print ' [!] At least specify a old and new IP to match in database!'
+			print ' [!] At least specify a old and new IP to match in database!\n'
 		else:
 			backend.replace_all_ip(values['old'], values['new'])
